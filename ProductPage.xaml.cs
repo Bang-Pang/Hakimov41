546b59d5bb091929fac077e0d077eaadc9af52ed
KarimovArslan <kariim06@mail.ru>
2025-11-24 17:58:47 +0500

    лаба 12

--- ProductPage.xaml.cs
+++ ProductPage.xaml.cs
@@ -31,32 +31,25 @@ namespace Hakimov41
 
         private void UpdateProduct()
         {
-            var currentProducts = Hakimov41Entities1.GetContext().Product.ToList();
+            var allProducts = Hakimov41Entities1.GetContext().Product.ToList();
 
-            if(ComboType.SelectedIndex == 0)
+            var currentProducts = allProducts;
+
+            if (ComboType.SelectedIndex == 0)
             {
                 currentProducts = currentProducts.Where(p => (Convert.ToInt32(p.ProductDiscountAmount) >= 0 && Convert.ToInt32(p.ProductDiscountAmount) <= 100)).ToList();
             }
-
             if (ComboType.SelectedIndex == 1)
             {
-                currentProducts = currentProducts.Where(p => (Convert.ToInt32(p.ProductDiscountAmount) >= 0 && Convert.ToInt32(p.ProductDiscountAmount) < 9.99)).ToList();
+                currentProducts = currentProducts.Where(p => (Convert.ToDouble(p.ProductDiscountAmount) >= 0 && Convert.ToDouble(p.ProductDiscountAmount) < 9.99)).ToList();
             }
             if (ComboType.SelectedIndex == 2)
             {
-                currentProducts = currentProducts.Where(p => (Convert.ToInt32(p.ProductDiscountAmount) >= 10 && Convert.ToInt32(p.ProductDiscountAmount) <= 14.99)).ToList();
+                currentProducts = currentProducts.Where(p => (Convert.ToDouble(p.ProductDiscountAmount) >= 10 && Convert.ToDouble(p.ProductDiscountAmount) <= 14.99)).ToList();
             }
             if (ComboType.SelectedIndex == 3)
             {
-                currentProducts = currentProducts.Where(p => (Convert.ToInt32(p.ProductDiscountAmount) >= 15 && Convert.ToInt32(p.ProductDiscountAmount) <= 49.99)).ToList();
-            }
-            if (ComboType.SelectedIndex == 4)
-            {
-                currentProducts = currentProducts.Where(p => (Convert.ToInt32(p.ProductDiscountAmount) >= 50 && Convert.ToInt32(p.ProductDiscountAmount) < 70)).ToList();
-            }
-            if (ComboType.SelectedIndex == 5)
-            {
-                currentProducts = currentProducts.Where(p => (Convert.ToInt32(p.ProductDiscountAmount) >= 70 && Convert.ToInt32(p.ProductDiscountAmount) <= 100)).ToList();
+                currentProducts = currentProducts.Where(p => (Convert.ToDouble(p.ProductDiscountAmount) >= 15 && Convert.ToDouble(p.ProductDiscountAmount) <= 100)).ToList();
             }
 
             currentProducts = currentProducts.Where(p => p.ProductName.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
@@ -66,12 +59,14 @@ namespace Hakimov41
             {
                 ProductListView.ItemsSource = currentProducts.OrderByDescending(p => p.ProductCost).ToList();
             }
-
             if (RButtonUp.IsChecked.Value)
             {
                 ProductListView.ItemsSource = currentProducts.OrderBy(p => p.ProductCost).ToList();
             }
+
+            CountTextBlock.Text = $"{currentProducts.Count} из {allProducts.Count}";
         }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Manager.MainFrame.Navigate(new AddEditPage());
